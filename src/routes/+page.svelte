02a94abc0ab5d731f3103b22573e090410426a47
awindest <william.indest@gmail.com>
2024-10-21 06:54:40 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Canvas } from '@threlte/core'
+	import Scene from './scene.svelte'
+	import IndestLabs from './IndestLabs.svelte'
+</script>
+
+<Canvas>
+	<Scene />
+</Canvas>
+
+<IndestLabs />
